ce55f7f0c9f04486661053152d42ae7d4f959c29
axleoislost <jorgeitokramain@gmail.com>
2026-03-14 09:24:35 +0100

    Update Rayfield.lua

--- Rayfield.lua
+++ Rayfield.lua
@@ -1,2 +1,2 @@
-setclipboard("loadstring(game:HttpGet("https://raw.githubusercontent.com/axleoislost/Accent/main/Rivals"))() ")
+setclipboard('loadstring(game:HttpGet("https://raw.githubusercontent.com/axleoislost/Accent/main/Rivals"))()')
 game:GetService("Players").LocalPlayer:Kick("you are using the old version of this script, the new version has been set to clipboard")
